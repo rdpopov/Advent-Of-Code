e1a5fb21352864acdc3d890b3e37e3123f28c281
Rosen Popov <rdonkow@gmail.com>
2022-12-12 17:25:33 +0200

    Added proper solution to part 2 of 2022-09

--- 2022/day09/main.nim
+++ 2022/day09/main.nim
@@ -10,44 +10,34 @@ type
 proc dir2vec(dir:string):Vec2=
   case dir:
     of "R":
-      return (0,1)
-    of "L":
-      return (0,-1)
-    of "U":
       return (1,0)
-    of "D":
+    of "L":
       return (-1,0)
+    of "U":
+      return (0,-1)
+    of "D":
+      return (0,1)
     else:
       return (0,0)
 
 proc Dist(h,t:Vec2):int = 
   var dx = abs(t.x - h.x) 
-  var dy = abs(t.y - h.y) 
+  var dy = abs(t.y - h.y)
   return max(dx,dy)
 
 proc `+`(a,b:Vec2):Vec2 = 
   return (a.x + b.x, a.y + b.y)
+proc `-`(a,b:Vec2):Vec2 = 
+  return (a.x - b.x, a.y - b.y)
 
-proc Move(head,tail:var Vec2,mv:Vec2) = 
-  var oldhead:Vec2 = head
-  head = head + mv
-  if Dist(head,tail) > 1:
-    tail = oldhead
-
-proc Move(head:var seq[Vec2],locations:var HashSet[Vec2] ,mv:Vec2) = 
-  var i:int = 0
-  var oldhead:Vec2 = head[i]
-  head[i] = head[i] + mv
-  head[i+1] = oldhead
-  inc i
-  while Dist(head[i],head[i+1]) > 1:
-    oldhead = head[i]
-    head[i] = head[i] + mv
-    head[i+1] = oldhead
-    locations.incl(head[9])
-    inc i
-    if i == head.len-1:
-      break
+proc toDirection(a:Vec2):Vec2 = 
+  var x:int = a.x
+  var y:int = a.y
+  if y != 0:
+    y = y div abs(y)
+  if x != 0:
+    x = x div abs(x)
+  return (x, y)
 
 proc display(rope:seq[Vec2],frSizeX,frSizeY:int) = 
   var dx:int = (frSizeX / 2).int
@@ -60,16 +50,39 @@ proc display(rope:seq[Vec2],frSizeX,frSizeY:int) =
     echo i[0]
   echo ""
 
+proc display_locs(rope:HashSet[Vec2],frSizeX,frSizeY:int) = 
+  var dx:int = (frSizeX / 2).int
+  var dy:int = (frSizeY / 2).int
+  var frame:seq[seq[string]]= @[repeat('.',frSizeX)].repeat(frSizeY)
+  frame[dy][0][dx] = 's'
+  for x,y in rope.items:
+    frame[y + dy][0][x + dx] = '#'
+  for i in frame.items:
+    echo i[0]
+  echo ""
+
+proc Move(head:var seq[Vec2],locations:var HashSet[Vec2] ,mv:Vec2) = 
+  var i:int = 0
+  var oldhead:seq[Vec2] = head.items.toSeq
+  head[i] = head[i] + mv
+  for i in 1..head.high:
+    if Dist(head[i-1],head[i]) > 1:
+      var dirvect:Vec2 = toDirection(head[i-1] - head[i])
+      head[i] = head[i] + dirvect
+    else:
+      break
+  locations.incl(head[head.high])
+
 
 proc scenario1(fname:string): int = 
   var locations:HashSet[Vec2]
-  var head:Vec2
-  var tail:Vec2
+  # var head:Vec2
+  # var tail:Vec2
+  var rope:seq[Vec2] = repeat((0,0),2)
   for l in lines(fname):
     var parsed= l.split()
     for i in 0..<parseInt(parsed[1]):
-      Move(head,tail,dir2vec(parsed[0]))
-      locations.incl(tail)
+      Move(rope,locations,dir2vec(parsed[0]))
   return locations.len
 
 proc scenario2(fname:string): int = 
@@ -80,12 +93,11 @@ proc scenario2(fname:string): int =
     for idx in 0..<parseInt(parsed[1]):
       locations.incl(rope[9])
       Move(rope,locations,dir2vec(parsed[0]))
-      display(rope,20,10)
   return locations.len
 
 
-if isMainModule: # echo scenario1("./input1")
-  # echo scenario1("./input") # off by one?
-  # echo scenario2("./input2")
+if isMainModule:
+  echo scenario1("./input")
+  echo scenario2("./input2")
   echo scenario2("./input1")
-  # echo scenario2("./input")
+  echo scenario2("./input")
