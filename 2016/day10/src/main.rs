f554f72edda7232b1c1379a40bfd93d8d208d881
Rosen Popov <rdonkow@gmail.com>
2023-04-14 23:14:24 +0300

    Added day 10 solution

--- 2016/day10/src/main.rs
+++ 2016/day10/src/main.rs
@@ -1,11 +1,6 @@
 use std::{collections::HashMap, fs, process::Output, ptr::hash};
 
-fn main() {
-    const INPUT_FILE: &str = "input_ex";
-    Part1(INPUT_FILE);
-}
-
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 enum Drain {
     Bot(usize),
     Output(usize),
@@ -17,7 +12,7 @@ impl Default for Drain {
     }
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug, Default, Clone)]
 struct Robot {
     input: Vec<u32>,
     low_idx: Drain,
@@ -61,26 +56,27 @@ fn build_rules(inp: &str) -> (HashMap<usize, Robot>, HashMap<usize, Vec<u32>>) {
             );
         }
     }
-    println!("{:#?}", result);
-    println!("{:#?}", outputs);
+    // println!("{:#?}", result);
+    // println!("{:#?}", outputs);
     return (result, outputs);
 }
 enum Roundresult {
     Played,
     NoStart,
 }
+// target is a tuple of indexes, needed for the solution,
+// -> which node compares 17 and 63?
+// -> also should be sorted
 fn playRound(
     automata: &mut HashMap<usize, Robot>,
     outputs: &mut HashMap<usize, Vec<u32>>,
-) -> Result<Roundresult,usize> {
-    let mut played: bool = false;
+    target: (u32, u32),
+) -> Result<usize, usize> {
     let mut current_bot_key: Vec<usize> = vec![];
-    let mut res = Ok(Roundresult::NoStart);
 
     for (k, v) in automata.into_iter() {
-        if v.input.len() == 2 {
+        if v.input.len() >= 2 {
             current_bot_key.push(*k);
-            res = Ok(Roundresult::Played);
             break;
         }
     }
@@ -89,33 +85,100 @@ fn playRound(
         if current_bot_key.len() > 0 {
             let bot = current_bot_key.remove(0);
 
-            let robot:&Robot = automata.get(&bot).unwrap().;
+            //get uinput
+            let mut robot: Robot = automata.get(&bot).unwrap().clone();
             // this is iffy V
-            let mut temp_array = robot.input.to_owned().into_iter().take(2).collect::<Vec<u32>>(); 
+            if robot.input.len() >= 2 {
+                robot.input.resize(2, 0);
 
-            temp_array.sort();
-            let min = temp_array[0];
-            let max = temp_array[1];
+                robot.input.sort();
+                let min = robot.input[0];
+                let max = robot.input[1];
+                if min == target.0 && max == target.1 {
+                    // We are just going to use the error propagation functionality of rust
+                    return Err(bot);
+                }
 
-            match robot.low_idx {
-                Drain::Bot(x) => automata.get_mut(&x).unwrap().input.push(min),
-                Drain::Output(x) => outputs.get_mut(&x).unwrap().push(min),
+                // update children
+                match robot.low_idx {
+                    Drain::Bot(x) => {
+                        // println!("from {bot} to {x} send {min}");
+                        automata.get_mut(&x).unwrap().input.push(min);
+                        current_bot_key.push(x);
+                    }
+                    Drain::Output(x) => outputs.get_mut(&x).unwrap().push(min),
+                }
+
+                match robot.high_idx {
+                    Drain::Bot(x) => {
+                        // println!("from {bot} to {x} send {max}");
+                        automata.get_mut(&x).unwrap().input.push(max);
+                        current_bot_key.push(x);
+                    }
+                    Drain::Output(x) => outputs.get_mut(&x).unwrap().push(max),
+                }
+                // remove first 2
+                automata.get_mut(&bot).unwrap().input.remove(0);
+                automata.get_mut(&bot).unwrap().input.remove(0);
+            } else {
+                continue;
             }
-
-            match robot.high_idx {
-                Drain::Bot(x) => automata.get_mut(&x).unwrap().input.push(max),
-                Drain::Output(x) => outputs.get_mut(&x).unwrap().push(max),
-            }
-
         } else {
             break;
         }
     }
-
-    res
+    Ok(0)
+}
+fn add_to_input(automata: &mut HashMap<usize, Robot>, value: u32, bot_key: usize) {
+    automata
+        .get_mut(&bot_key)
+        .expect("How did we miss this bot?")
+        .input
+        .push(value);
 }
 
-fn Part1(input_file: &str) {
+fn Part1(input_file: &str, target: (u32, u32)) -> Result<usize, usize> {
     let inp = fs::read_to_string(input_file).expect("Sohuld be able to read file");
-    let (automata, outputs) = build_rules(&inp);
+    let (mut automata, mut outputs) = build_rules(&inp);
+    for l in inp.lines() {
+        let mut tok = l.split(' ');
+        match tok.nth(0).unwrap() {
+            "bot" => (),
+             "value"=> add_to_input(
+                &mut automata,
+                tok.nth(0).unwrap().parse::<u32>().unwrap(),
+                tok.nth(3).unwrap().parse::<usize>().unwrap()
+            ),
+            _ => (),
+        }
+        playRound(&mut automata, &mut outputs, target)?;
+    }
+
+    return Ok(0);
+}
+
+fn Part2(input_file: &str, target: (u32, u32)) -> Result<usize, usize> {
+    let inp = fs::read_to_string(input_file).expect("Sohuld be able to read file");
+    let (mut automata, mut outputs) = build_rules(&inp);
+    for l in inp.lines() {
+        let mut tok = l.split(' ');
+        match tok.nth(0).unwrap() {
+            "bot" => (),
+            "value"=> add_to_input(
+                &mut automata,
+                tok.nth(0).unwrap().parse::<u32>().unwrap(),
+                tok.nth(3).unwrap().parse::<usize>().unwrap()
+            ),
+            _ => (),
+        }
+        playRound(&mut automata, &mut outputs, target)?;
+    }
+    println!("{:#?}",&outputs.get(&0).unwrap()[0] * &outputs.get(&1).unwrap()[0] * &outputs.get(&2).unwrap()[0] );
+
+    return Ok(0);
+}
+fn main() {
+    const INPUT_FILE: &str = "input";
+    println!("{:?}",Part1(INPUT_FILE, (17, 61)));
+    println!("{:?}",Part2(INPUT_FILE, (0, 0)));
 }
