8b7ff197a3d23b5bdc01c4571b78d7da05e6d62d
Rosen Popov <rdonkow@gmail.com>
2023-05-29 09:51:08 +0300

    Added part of day19 2016 solution

--- 2016/day19/src/main.rs
+++ 2016/day19/src/main.rs
@@ -3,6 +3,21 @@ fn Part1(elves:usize) -> usize {
     return ((elves & !(1 << elves.ilog2())) << 1 ) + 1 as usize;
 }
 
+fn Part2(elves_n:usize) -> usize {
+    let mut elves:Vec<usize> = vec![0;elves_n];
+    let crnt_idx:usize = 0;
+    for (i,el_n) in elves.iter_mut().enumerate(){
+        *el_n = i;
+    }
+    while elves.len() > 0{
+        let opposite_idx = crnt_idx + elves.len()/2;
+
+            crnt_idx +=1;
+    }
+    0
+
+}
+
 fn main() {
     println!("{}",Part1(3017957));
 }
