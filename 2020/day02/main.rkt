c06284856869ba0180303a4ed43bf0a06c8546af
Rosen Popov <rpopov@mm-sol.com>
2023-05-29 18:56:33 +0300

    Partial updated on day 2 of 2020 in scheme/racket

--- 2020/day02/main.rkt
+++ 2020/day02/main.rkt
@@ -1,9 +1,31 @@
 #lang racket
+(require racket/local)
 
 (define inp-file "./input")
-(define (task-input fname) (map string->number (file->lines fname)))
+(define (task-input fname) (file->lines fname))
 
 (define (parse-line l)
-  (string-split l #rx":|-| " )
+  (remove "" (string-split l #rx":|-| " ))
   )
-(print (parse-line "2-9 c: ccccccccc"))
+
+
+
+(define (verify-password ln)
+  (define pas (parse-line ln))
+  #| (define counted (- (apply string-length (cdddr pas)) |#
+  #|                      (string-length (remove (caddr pas) (cdddr pas))) |#
+  #|                      )) |#
+  (print (apply remove (cadr) (cdddr pas)))
+  #| (println (apply string-length (cdddr pas))) |#
+  #| (println (apply string-length (remove (caddr pas) (cadddr pas)))) |#
+  (cond
+    [(< (string->number (car pas)) 0 (string->number (cadr pas))) 1]
+    [true 0]
+    )
+  )
+
+(define (part1 inp) (foldr + 0 (map verify-password inp)))
+#| (print (part1 (task-input inp-file))) |#
+(print (verify-password "2-9 c: ccccccccc" ))
+(print (verify-password "1-3 a: abcde" ))
+
