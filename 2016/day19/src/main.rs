cb73835dc5c25fea1ad1f930ec39b61925ba2a8c
Rosen Popov <rpopov@mm-sol.com>
2023-05-29 12:23:12 +0300

    Added day 19 of 2016 in rust

--- 2016/day19/src/main.rs
+++ 2016/day19/src/main.rs
@@ -4,20 +4,17 @@ fn Part1(elves:usize) -> usize {
 }
 
 fn Part2(elves_n:usize) -> usize {
-    let mut elves:Vec<usize> = vec![0;elves_n];
-    let crnt_idx:usize = 0;
-    for (i,el_n) in elves.iter_mut().enumerate(){
-        *el_n = i;
+    let  mut w = 1;
+    for i in 1..elves_n {
+        w = w % i + 1;
+        if w > (i + 1)/2{
+            w+=1;
+        }
     }
-    while elves.len() > 0{
-        let opposite_idx = crnt_idx + elves.len()/2;
-
-            crnt_idx +=1;
-    }
-    0
-
+    return w;
 }
 
 fn main() {
     println!("{}",Part1(3017957));
+    println!("{}",Part2(3017957));
 }
