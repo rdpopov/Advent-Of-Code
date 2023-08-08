f59283d40cf038449494069bc4f8eb3154376503
Rosen Popov <50305462+rdpopov@users.noreply.github.com>
2023-08-08 17:33:41 +0300

    Added day 22 part 2 of 2016 in rust

--- 2016/day22/src/main.rs
+++ 2016/day22/src/main.rs
@@ -2,6 +2,11 @@ use std::{fs, iter::Empty, cmp::max};
 
 const USED: usize = 2;
 const AVAIL: usize = 3;
+
+#[test]
+fn test_part1(){
+    assert_eq!(1003, part1("input_str"));
+}
 fn part1(input_file: &str) -> usize {
     let inp = fs::read_to_string(input_file).expect("cant acess file");
     let mut sizes: Vec<(i32, i32)> = vec![];
@@ -41,6 +46,17 @@ fn visuzlize (matr:&Vec<Vec<(i32,i32)>>,maxX:usize,maxY:usize,empty_size:i32) {
     }
 }
 
+// no need for a star . just count it .... and then its formulaic to move the data 1 pace and for
+// the empty to be in the same position relative to it, it takes 5 moves =>
+// and the final takes 2 moves because it doesnt need to be afull cycle.
+// the total amount of moves would be the ((length-1)        -1)    *   5                     + 2            + 25  
+//                                         we start here    for final   for each link moved    for the final   to move the empty in position
+//
+#[test]
+fn test_part2(){
+    assert_eq!(192, part2("input_str"));
+}
+
 fn part2(input_file: &str) -> usize {
     let inp = fs::read_to_string(input_file).expect("cant acess file");
     let mut sizes: Vec<Vec<(i32, i32)>> = vec![vec![(0,0);40];40];
@@ -66,10 +82,8 @@ fn part2(input_file: &str) -> usize {
         println!("{:?}",tmp);
         sizes[y][x] = (sz,taken);
     }
-    let emplace_empty_movments = (maxX - empty_x_y.0 - 1)  + (empty_x_y.1 - 1);
-    let move_almost_there = (maxX ) * 4;
-    let move_final = 2;
-    let movments = 27 + move_almost_there + move_final;
+    let move_almost_there = (maxX - 2 ) * 5 + 2;
+    let movments = 25 + move_almost_there ;
 
     visuzlize(&sizes, maxX, maxY, empty_size);
     movments
@@ -77,7 +91,6 @@ fn part2(input_file: &str) -> usize {
 
 
 fn main() {
-    // println!("part 1 {}", part1("input_str"));
-    println!("part 1 {}", part2("input_str"));
-    // println!("part 1 {}", part2("input1_src"));
+    println!("Part 1 {}", part1("input_str"));
+    println!("Part 2 {}", part2("input_str"));
 }
