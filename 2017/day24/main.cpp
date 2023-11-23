a57dbd60cc31414a9b32ac07b270bc9284548702
Rosen Popov <rdonkow@gmail.com>
2023-11-23 18:33:02 +0200

    Added day 24 of 2017 in c++ solved

--- 2017/day24/main.cpp
+++ 2017/day24/main.cpp
@@ -18,6 +18,7 @@
 #include <istream>
 #include <string>
 #include <sys/types.h>
+#include <utility>
 #include <vector>
 #include <map>
 #include <queue>
@@ -26,11 +27,11 @@
 
 #define pr(V) { for(auto a:V) std::cout<< a << " "; std::cout<< "\n"; }
 
-int calc(std::vector<int> &route, std::map<int,std::vector<int>> &nodes){
+std::pair<int,int> calc(std::vector<int> &route, std::map<int,std::vector<int>> &nodes){
     int ret = 0;
     std::set<int> loops;
     if (route.size() == 1){
-        return 0;
+        return {0,0};
     };
     for (auto a:route){
         ret += 2*a;
@@ -47,7 +48,7 @@ int calc(std::vector<int> &route, std::map<int,std::vector<int>> &nodes){
     }
     ret -= route[0];
     ret -= route[route.size()-1];
-    return ret;
+    return {ret,route.size() + loops.size()};
 }
 
 bool in_vec(int n, std::vector<int> &route){
@@ -58,19 +59,31 @@ bool in_vec(int n, std::vector<int> &route){
     }
     return false;
 }
+bool pair_in_vec(std::pair<int,int> p, std::vector<int> &route){
+    if(p.first == p.second) return true;
+    for(int i = 1; i < route.size();i++)
+    {
+        if(p.first == route[i-1] &&
+           p.second == route[i] ||
+           p.first == route[i] &&
+           p.second == route[i-1]) 
+            return true;
+    }
+    return false;
+}
 
-int traverse(int start,std::map<int,std::vector<int>> &nodes){
+int traverse_part1(int start,std::map<int,std::vector<int>> &nodes){
     std::queue<std::vector<int>> que;
     std::vector<int> max_path;
     int max_cost = 0;
     que.push({start});
     while (que.size()) {
         std::vector<int> crnt = que.front();
-        /* pr(crnt); */
         //see all the children of the last elemnt visited
         bool final = true;
-        for(auto a: nodes[crnt[crnt.size()-1]]) {
-            if (!in_vec(a,crnt)){
+        int parent = crnt[crnt.size()-1];
+        for(auto a: nodes[parent]) {
+            if (!pair_in_vec({a,parent},crnt)){
                 std::vector<int> new_path(crnt);
                 new_path.push_back(a);
                 que.push(new_path);
@@ -78,13 +91,41 @@ int traverse(int start,std::map<int,std::vector<int>> &nodes){
             }
         }
         if(final) {
-            max_cost = std::max(max_cost,calc(crnt,nodes));
-            std::cout<<max_cost << " -> ";
-            pr(crnt);
+            max_cost = std::max(max_cost,calc(crnt,nodes).first);
         }
         que.pop();
     }
     return max_cost;
+}
+
+int traverse_part2(int start,std::map<int,std::vector<int>> &nodes){
+    std::queue<std::vector<int>> que;
+    std::vector<int> max_path;
+    std::pair<int,int> max_cost = {0,0};
+    que.push({start});
+    while (que.size()) {
+        std::vector<int> crnt = que.front();
+        bool final = true;
+        int parent = crnt[crnt.size()-1];
+        for(auto a: nodes[parent]) {
+            if (!pair_in_vec({a,parent},crnt)){
+                std::vector<int> new_path(crnt);
+                new_path.push_back(a);
+                que.push(new_path);
+                final = false;
+            }
+        }
+        if(final) {
+            std::pair<int,int> metric = calc(crnt,nodes);
+            if (metric.second > max_cost.second){
+                max_cost = metric;
+            } else if (metric.second == max_cost.second && metric.first > max_cost.first ){
+                max_cost = metric;
+            }
+        }
+        que.pop();
+    }
+    return max_cost.first;
 }
 
 int part1(std::string fname) {
@@ -112,19 +153,44 @@ int part1(std::string fname) {
         }
     }
 
-    for (auto a : nodes) {
-        std::cout << a.first << " -> ";
-        pr(a.second);
+    return traverse_part1(0, nodes);
+}
+
+int part2(std::string fname) {
+    std::ifstream in(fname);
+    std::string line;
+    std::map<int,std::vector<int>> nodes;
+    int max_cost = 0;
+
+    while(std::getline(in,line)){
+        int from;
+        int to;
+        sscanf(line.c_str(),"%d/%d" ,&from,&to);
+        if (nodes.find(to) == nodes.end()){
+            nodes[to] = {};
+        }
+        if (nodes.find(from) == nodes.end()){
+            nodes[from] = {};
+        }
+
+        if (from == to){
+            nodes[to].push_back(to);
+        } else {
+            nodes[to].push_back(from);
+            nodes[from].push_back(to);
+        }
     }
-    return traverse(0, nodes);
+
+    return traverse_part2(0, nodes);
 }
 
 
 
-
 int main (int argc, char *argv[]) {
-    /* std::cout << "Part 1 -> "<< part1("./test") << std::endl; */
+    std::cout << "Part 1 -> "<< part1("./test") << std::endl;
     std::cout << "Part 1 -> "<< part1("./input") << std::endl;
+    std::cout << "Part 2 -> "<< part2("./test") << std::endl;
+    std::cout << "Part 2 -> "<< part2("./input") << std::endl;
 
     return 0;
 }
