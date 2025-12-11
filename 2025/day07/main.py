0ad9c15bfbee4ad9fc569ac04756215416441e55
Rosen Popov <rdonkow@gmail.com>
2025-12-11 15:42:03 +0200

    Added day 7 part 2 of 2025

--- 2025/day07/main.py
+++ 2025/day07/main.py
@@ -2,7 +2,7 @@ import numpy as np
 import math as m
 
 
-def transform(l1,l2):
+def transform(l1,l2,matr):
     spl = 0
     for  i in  range(len(l2)-2):
         s1 = ''.join(l1[i:i+3])
@@ -12,46 +12,49 @@ def transform(l1,l2):
             l2[i+1] = "|"
 
         if s1[1] == 'S' and s2[1] == '.':
+            matr[i+1] = 1
             l2[i+1] = "|"
 
         if s1[1] == '|' and s2[1] == '^':
+            spl +=1
+            matr[i] += matr[i+1]
+            matr[i+2] += matr[i+1]
+            matr[i+1] = 0
             if s2[0] == '.':
                 l2[i] = "|"
                 spl +=1
             if s2[2] == '.':
                 l2[i+2] = "|"
-                spl +=1
-
-
     return spl
-
 
 def itr(field) -> int:
+    spl = 0
+    matr = np.zeros(len(field[0]))
     for i in range(len(field)):
-    # for i in range(len(field)):
         if i == 0 or i == len(field)-1:
             continue
-        transform(field[i-1], field[i])
-        # print(''.join(field[i]),s)
-    spl = 0
-    for i in range(len(field)-1):
-        for j in range(len(field[i])):
-            if field[i][j] == '|' and field[i+1][j] == '^':
-                spl += 1
-    return spl
-    
+        spl+=transform(field[i-1], field[i],matr)
+    return spl,int(sum(matr))
 
 def part1(file: str) -> int:
     res = 0
     with open(file, 'r') as f:
         lines = [list(line.rstrip()) for line in f]
-        res = itr(lines)
-        
+        res,_ = itr(lines)
         return file,res
 
+
+def part2(file: str) -> int:
+    res = 0
+    with open(file, 'r') as f:
+        lines = [list(line.rstrip()) for line in f]
+        _,res = itr(lines)
+        return file,res
 def main():
     print( part1('ex'))
     print( part1('input'))
 
+    print( part2('ex'))
+    print( part2('input'))
 if __name__ == "__main__":
     main()
